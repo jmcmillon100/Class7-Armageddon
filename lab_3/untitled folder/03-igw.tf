bc1152a952a227367121b9bd7242c1e5803d104f
jmcmillon100 <144070832+jmcmillon100@users.noreply.github.com>
2026-02-10 10:13:34 -0500

    update

--- lab_3/untitled folder/03-igw.tf
+++
@@ -1,9 +0,0 @@
-
-
-resource "aws_internet_gateway" "dawgs_igw01" {
-  vpc_id = aws_vpc.dawgs_vpc01.id
-
-  tags = {
-    Name = "${local.name_prefix}-igw01"
-  }
-}
--- lab_3/untitled folder/22a-origin-header.tf
+++
@@ -1,8 +0,0 @@
-############################################
-# Origin Cloaking Secret Header
-############################################
-
-resource "random_password" "dawgs_origin_header_value01" {
-  length  = 32
-  special = false
-}
